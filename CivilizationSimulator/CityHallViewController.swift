ce459d7db41988b47108568a1cfe98ca1d9133d9
Alan Tokarsky 2019 <atokars9@3012-l29537-s.local>
2019-05-09 10:05:33 -0500

    view controllers

---
+++ CivilizationSimulator/CityHallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CityHallViewController.swift
+//  CivilizationSimulator
+//
+//  Created by Alan Tokarsky 2019 on 5/9/19.
+//  Copyright © 2019 Alex Beckerman 2019. All rights reserved.
+//
+
+import UIKit
+
+class CityHallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
